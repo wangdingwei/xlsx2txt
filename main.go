ef3f8e22336962993c294704e1f31855c67409a6
wdw <wdw@82696.com>
2023-03-27 20:55:10 +0800

    output file as arg

--- main.go
+++ main.go
@@ -1,56 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 
 	"github.com/xuri/excelize/v2"
 )
 
+var writer *bufio.Writer
+
 func main() {
 
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s filename", os.Args[0])
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s infile outfile", os.Args[0])
 		fmt.Fprintln(os.Stderr)
 		os.Exit(1)
 	}
 
-	file := os.Args[1];
-	//fmt.Println(file)
+	infileName := os.Args[1];
+	outfileName := os.Args[2];
+	// fmt.Println(infileName, outfileName)
 
-    f, err := excelize.OpenFile(file)
+    outfile, err := os.Create(outfileName)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    defer outfile.Close()
+
+    writer = bufio.NewWriter(outfile)
+
+    xlsxFile, err := excelize.OpenFile(infileName)
     if err != nil {
         fmt.Fprintln(os.Stderr, "open failed:", err)
 		os.Exit(1)
     }
     defer func() {
-        if err := f.Close(); err != nil {
+        if err := xlsxFile.Close(); err != nil {
 			fmt.Fprintln(os.Stderr, err)
         }
     }()
 
 
-	for _, sheet := range f.GetSheetList() {
+	for _, sheet := range xlsxFile.GetSheetList() {
         
-        println()
-		println("======", sheet, "======")
-        printSheet(f, sheet)
-        println()
+        fmt.Fprintln(writer)
+	    fmt.Fprintln(writer, "======", sheet, "======")
+        printSheet(xlsxFile, sheet)
+        fmt.Fprintln(writer)
 	}
-	os.Exit(0)
-
+    writer.Flush()
 }
 
-func printSheet(f *excelize.File, sheet string) {
-    rows, err := f.GetRows(sheet)
+func printSheet(xlsxFile *excelize.File, sheet string) {
+    rows, err := xlsxFile.GetRows(sheet)
     if err != nil {
         fmt.Println(err)
         return
     }
     for _, row := range rows {
-        for _, colCell := range row {
-            fmt.Print(colCell, "|")
+        len := len(row);
+        for idx, cell := range row {
+            fmt.Fprint(writer, cell)
+            if idx < len - 1 { // last
+                fmt.Fprint(writer, "|")
+            }
         }
-        fmt.Println()
+        fmt.Fprintln(writer)
     }
 }
