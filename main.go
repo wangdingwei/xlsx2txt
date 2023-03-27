344d02fc151780eeaf50941de8de3d4741bd8d80
wdw <wdw@82696.com>
2023-03-27 17:22:43 +0800

    init

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	}
 
 	file := os.Args[1];
-	fmt.Println(file)
+	//fmt.Println(file)
 
     f, err := excelize.OpenFile(file)
     if err != nil {
@@ -24,7 +24,6 @@ func main() {
 		os.Exit(1)
     }
     defer func() {
-        // Close the spreadsheet.
         if err := f.Close(); err != nil {
 			fmt.Fprintln(os.Stderr, err)
         }
@@ -32,26 +31,25 @@ func main() {
 
 
 	for _, sheet := range f.GetSheetList() {
-		println(sheet)
+        
+        println()
+		println("======", sheet, "======")
+        printSheet(f, sheet)
+        println()
 	}
 	os.Exit(0)
 
-    // Get value from cell by given worksheet name and cell reference.
-    cell, err := f.GetCellValue("Sheet1", "B2")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(cell)
-    // Get all the rows in the Sheet1.
-    rows, err := f.GetRows("Sheet1")
+}
+
+func printSheet(f *excelize.File, sheet string) {
+    rows, err := f.GetRows(sheet)
     if err != nil {
         fmt.Println(err)
         return
     }
     for _, row := range rows {
         for _, colCell := range row {
-            fmt.Print(colCell, "\t")
+            fmt.Print(colCell, "|")
         }
         fmt.Println()
     }
